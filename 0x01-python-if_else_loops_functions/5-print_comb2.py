2ca869cb6a0da1bf561e22065a91c1ecd5d53596
BrightOlawale <oolatunjibright@yahoo.com>
2022-06-01 02:25:27 +0000

    Task 5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 for i in range(100):
+    if i == 99:
+        print("{}\n".format(i))
+        break
     if i <= 9:
         print("0", end="")
     print("{}".format(i), end=", ")
