af1da384a00d8285d7e6e59ba66c4d3d1d599709
BrightOlawale <oolatunjibright@yahoo.com>
2022-06-02 21:19:01 +0000

    Task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,8 +3,12 @@ from sys import argv
 
 if __name__ == "__main__":
     vc = len(argv)
-    if vc == 1:
-        print("{} arguments.".format(0))
+    if vc <= 2:
+        if vc == 1:
+            print("{} argument.".format(0))
+        else:
+            print("{} argument:".format((vc-1)))
+            print("{}: {}".format((vc-1), argv[vc-1]))
     else:
         print("{} arguments:".format((vc-1)))
         for i in range(1, vc):
