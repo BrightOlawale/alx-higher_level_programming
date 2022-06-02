65e6678497bf331bc17c82def57167e587931769
BrightOlawale <oolatunjibright@yahoo.com>
2022-06-02 22:25:07 +0000

    Task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-import hidden_4 as hd
+import hidden_4
 
 if __name__ == "__main__":
-    for j in dir(hd):
+    for j in dir(hidden_4):
         if "__" not in j:
             print(j)
 
