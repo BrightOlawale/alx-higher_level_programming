16cc6daf628a826f01a2cf922f048228ebfe449b
BrightOlawale <oolatunjibright@yahoo.com>
2022-06-07 15:32:55 +0000

    Calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -19,4 +19,3 @@ if __name__ == "__main__":
         for i in range(len(ops)):
             if argv[2] == ops[i]:
                 print("{} + {} = {}".format(argv[1], argv[3], func[i]))
-                exit(0)
