800a6d93a3a552dca57ffc4dfc6a7f52f509fa3b
BrightOlawale <oolatunjibright@yahoo.com>
2022-06-07 00:01:08 +0000

    max and return

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,7 +2,10 @@
 
 
 def max_integer(my_list=[]):
-    maxim = 0
+    if len(my_list) !> 0:
+        maxim = None
+    else:
+        maxim = 0
     for elem in my_list:
         if elem > maxim:
             maxim = elem
