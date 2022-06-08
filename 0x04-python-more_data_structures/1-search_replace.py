1f9a381383f223e97afb2004a3db1fcbb92a861e
BrightOlawale <oolatunjibright@yahoo.com>
2022-06-08 17:08:18 +0000

    Search and replace

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,9 @@
+#!/usr/bin/pyhton3
+
+
+def search_replace(my_list, search, replace):
+    newlist = my_list[:]
+    for idx, i in enumerate(newlist):
+        if i == search:
+            newlist[idx] = replace
+    return newlist
