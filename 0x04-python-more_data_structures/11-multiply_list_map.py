bcf2299bbc4ee64be3ae840ace41e6f474df1ae4
BrightOlawale <oolatunjibright@yahoo.com>
2022-06-11 03:19:04 +0000

    Done

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
-def multiply_by_2(my_dict):
-    new_dict = {}
-    if my_dict:
-        for key, val in my_dict.items():
-            new_dict.update({key: val * 2})
-        return (new_dict)
+def mutiply_list_map(my_list=[], number=0):
+    return (list(map(lambda x: x * number, my_list)))
