18e1a75f945c2484eb3b589d90c1799ec3ded28d
Bright Olawale <oolatunjibright@yahoo.com>
2022-06-22 11:18:01 +0100

    Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''a class Square that defines a square by: (based on 0-square.py)'''
+
+
+class Square:
+    '''Instantiation with size (no type/value verification)'''
+    __size = 0
+    def __init__(self, __size):
+        self._size = __size
+    pass
