3daf5be11dfccfff097c7f24f083f50a1bfe19d3
Bright Olawale <oolatunjibright@yahoo.com>
2022-07-05 12:30:29 +0100

    Instance check

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+    Module to compare class instances.
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+        Check if the object is of the same class.
+    """
+    return obj.__class__ == a_class
