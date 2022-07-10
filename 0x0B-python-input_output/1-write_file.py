1b1e40e70a34a2b87378102ca605266655cb6887
Bright Olawale <oolatunjibright@yahoo.com>
2022-07-10 03:13:57 +0100

    Write files

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Write a function that reads a text file (UTF8) and prints it to stdout.
+"""
+
+
+def write_file(filename="", text=""):
+    """
+    Writes a text to a file
+    """
+    with open(filename, "w", encoding="utf-8") as f:
+        f.write(text)
