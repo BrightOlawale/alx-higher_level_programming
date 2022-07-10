191217cfbcebf0cd7f99372c3262c2dd305f0f3c
Bright Olawale <oolatunjibright@yahoo.com>
2022-07-10 03:16:58 +0100

    Write files

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -9,4 +9,4 @@ def write_file(filename="", text=""):
     Writes a text to a file
     """
     with open(filename, "w", encoding="utf-8") as f:
-        f.write(text, end="")
+        return f.write(text)
