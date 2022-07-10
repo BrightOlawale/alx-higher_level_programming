239300caaa715829f784ee7e26e030571a84e131
Bright Olawale <oolatunjibright@yahoo.com>
2022-07-10 04:02:49 +0100

    Json files

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+    JSON representation of an object (string)
+"""
+
+
+def to_json_string(my_obj):
+    """
+    Returns a JSON string representation of an object
+    """
+    return json.dumps(my_obj)
