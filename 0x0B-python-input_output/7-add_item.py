e08ce9b2beca38566b83f27d215cd90765ef13a2
Bright Olawale <oolatunjibright@yahoo.com>
2022-07-11 12:59:32 +0100

    adding to json

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,21 @@
+#!/usr/bi/python3
+"""
+    script to add all arguments to a list
+"""
+
+
+import sys
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+
+filename = sys.argv[1:]
+try:
+    new_file = load_from_json_file("add_item.json")
+    new_file.append(filename)
+    save_to_json_file(new_file, "add_item.json")
+
+except FileNotFoundError:
+    save_to_json_file(filename, "add_item.json")
