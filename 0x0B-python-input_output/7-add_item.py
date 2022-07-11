bfb489f93832bda1ba51e565c4dc600b38ddf8f7
Bright Olawale <oolatunjibright@yahoo.com>
2022-07-11 13:01:39 +0100

    adding to json

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,4 +1,4 @@
-#!/usr/bi/python3
+#!/usr/bin/python3
 """
     script to add all arguments to a list
 """
@@ -6,9 +6,9 @@
 
 import sys
 
-save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
-load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
 filename = sys.argv[1:]
