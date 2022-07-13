5a8e944a8753467a4cf3ad3c0fe0ab1cf93201c2
Bright Olawale <oolatunjibright@yahoo.com>
2022-07-13 17:10:06 +0100

    __str__ function addded

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -69,3 +69,6 @@ class Rectangle(Base):
         """display function"""
         for col in range(self.__height):
             print("{:s}".format("#" * self.__width))
+
+    def __str__(self):
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.__x, self.__y, self.__width, self.__height)
