1eae866e1134e7d82b481e817c08580f0c39e8c3
Bright Olawale <oolatunjibright@yahoo.com>
2022-10-03 20:59:24 +0100

    orm list states

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 """Exercise"""
-from sqlalchemy import create_engine
-from model_state import Base, State
-from sqlalchemy.orm import sessionmaker
-import sys
 
 
 if __name__ == '__main__':
+    from sqlalchemy import create_engine
+    from model_state import Base, State
+    from sqlalchemy.orm import sessionmaker
+    import sys
+
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
         sys.argv[1], sys.argv[2], sys.argv[3], pool_pre_ping=True))
 
