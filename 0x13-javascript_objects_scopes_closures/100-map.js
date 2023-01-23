01271f99e16c62e909dbc233a6bbe9431ae4ca06
Bright Olawale <oolatunjibright@yahoo.com>
2023-01-23 11:14:40 +0100

    more tasks done

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-imported  = require ( './100-data.js' ) ;
-console . log ( imported . list ) ;
-console . log ( imported . list . map ( ( x ,  i )  =>  x  *  i ) ) ;
+imported  = require( './100-data.js' ) ;
+console.log ( imported.list ) ;
+console.log ( imported.list.map( ( x ,  i )  =>  x  *  i ) ) ;
