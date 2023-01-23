b5392f4978e512a7ef01f3f3642b901497db322c
Bright Olawale <oolatunjibright@yahoo.com>
2023-01-23 15:16:47 +0100

    [fix]: URl response fix

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,16 +1,12 @@
 #!/usr/bin/python3
-"""what's my status"""
-import urllib.request
-
-
-def myStatus():
-    """what's my status"""
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-        html = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(html)))
-        print("\t- content: {}".format(html))
-        print("\t- utf8 content: {}".format(html.decode('utf-8')))
+""" Python script that fetches https://alx-intranet.hbtn.io/status """
 
 if __name__ == "__main__":
-    myStatus()
+    import urllib.request as request
+    with request.urlopen("https://alx-intranet.hbtn.io/status") as response:
+        if response:
+            html = response.read()
+            print("Body response:")
+            print("\t- type: {}".format(type(html)))
+            print("\t- content: {}".format(html))
+            print("\t- utf8 content: {}".format(html.decode("utf-8")))
