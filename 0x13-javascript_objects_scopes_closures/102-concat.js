446c227b35de3a203790f0d5246ae23794f1be8f
BrightOlawale <oolawalebright@gmail.com>
2023-07-21 16:11:56 +0100

    feat: concat.js

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
 const fs = require('fs');
+const argv = process.argv;
 
-const fileOne = fs.readFileSync(process.argv[2], 'utf8');
-const fileTwo = fs.readFileSync(process.argv[3], 'utf8');
-
-fs.writeFileSync(process.argv[4], fileOne + fileTwo);
+const file1 = fs.readFileSync(argv[2], 'utf-8').toString();
+const file2 = fs.readFileSync(argv[3], 'utf-8').toString();
+fs.writeFileSync(argv[4], file1 + file2);
